242008c3b976c875fd6fefe209168841309a6037
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-05-24 16:43:58 +0200

    Fix correct version, mergesort-dllr version 1.1.0

--- binary-search-tree/tree.js
+++ binary-search-tree/tree.js
@@ -3,7 +3,7 @@ import mergeSort from 'mergesort-dllr';
 export function Tree(array, root) {}
 
 export function buildTree(array) {
-  let uniqueArray = [...new Set(array)];
+  let uniqueArray = [...new Set(array)]; // remove duplicates
   let sortedArray = mergeSort(uniqueArray);
   return sortedArray;
 }
